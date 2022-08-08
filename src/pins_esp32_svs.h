079524fffd387b1b5955912ec268158465d527f9
Marvin Banse <marvin@banse.biz>
2022-08-08 17:59:03 +0200

    define esp32-svs-pins

---
+++ src/pins_esp32_svs.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#define PIN_PWR 4
+
+#define PIN_I1 39
+#define PIN_I2 34
+#define PIN_I3 35
+#define PIN_I4 14
+#define PIN_O1 2
+#define PIN_O2 12
+#define PIN_O3 32
+#define PIN_O4 33
+
+#define PIN_MOSI 23
+#define PIN MISO 19
+#define PIN_CLK 18
+#define PIN_CS1 5
+#define PIN_CS2 15
+
+#define PIN_IO11 13
+#define PIN_IO12 26
+#define PIN_IO21 25
+#define PIN_IO22 27
+
+#define PIN_RX1 21
+#define PIN_TX1 22
+#define PIN_RX2 16
+#define PIN_TX2 17
